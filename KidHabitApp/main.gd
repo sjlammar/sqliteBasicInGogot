9a58b1e2f6cbf7e210cc57171b91c628b913e948
Ammar Sajili <103972530+sjlammar@users.noreply.github.com>
2024-06-13 14:50:22 +0700

    Add files via upload

--- KidHabitApp/main.gd
+++ KidHabitApp/main.gd
@@ -2,21 +2,47 @@ extends Control
 @onready var data = DataBase.database
 var totHabit
 var totTask
-
+var doneH
+var doneT
+var habit
+var task
 
 func _ready():
-	var count_query = "SELECT COUNT(*) as count FROM habit_tipe"
+	var count_query = "SELECT COUNT(*) as count FROM habit WHERE idUser=="+str(DataBase.idsession)
 	data.query(count_query)
 	totHabit = data.query_result[0]["count"]
 	
-	count_query = "SELECT COUNT(*) as count FROM task"
+	count_query = "SELECT COUNT(*) as count FROM habit WHERE idUser=="+str(DataBase.idsession)+" AND status == 1"
+	data.query(count_query)
+	doneH = data.query_result[0]["count"]
+	
+	count_query = "SELECT COUNT(*) as count FROM listTask WHERE idUser=="+str(DataBase.idsession)
 	data.query(count_query)
 	totTask = data.query_result[0]["count"]
 	
-	#insertRow()
+	count_query = "SELECT COUNT(*) as count FROM listTask WHERE idUser=="+str(DataBase.idsession)+" AND status == 1"
+	data.query(count_query)
+	doneT = data.query_result[0]["count"]
 	
-	$gridHabit/habit.text = str(totHabit)
-	$ColorRect/task.text = str(totTask)
+	habit = str(doneH)+"/"+str(totHabit)
+	task = str(doneT)+"/"+str(totTask)
+	
+	$GridContainer/jHabit.text = habit
+	$GridContainer/jTask.text = task
+	
+	var check_query = "SELECT COUNT(*) as count FROM status WHERE idUser = %s;"%DataBase.idsession
+	var result = data.query(check_query)
+	
+	if result :
+		var row = data.query_result[0]
+		var admin_count = row["count"]
+		
+		if admin_count == 0:
+			data.insert_row("status", {"idUser":DataBase.idsession,"name":"habit","jumlah":str(doneH)+"/"+str(totHabit)})
+			data.insert_row("status", {"idUser":DataBase.idsession,"name":"task","jumlah":str(doneT)+"/"+str(totTask)})
+	
+	data.update_rows("status", "idUser="+str(DataBase.idsession)+" AND name='habit'",{"jumlah":habit})
+	data.update_rows("status", "idUser="+str(DataBase.idsession)+" AND name='task'",{"jumlah":task})
 
 
 func _process(delta):
